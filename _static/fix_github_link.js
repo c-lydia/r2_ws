f940d21d45dd85bbed29d8d133cccfa9eb7f5125
Lydiya Chheng <chhenglydiacl@gmail.com>
2026-02-27 12:46:30 +0700

    Update documentation

--- _static/fix_github_link.js
+++ _static/fix_github_link.js
@@ -3,8 +3,8 @@
   try {
     var anchors = document.querySelectorAll('a.fa.fa-github');
     anchors.forEach(function(a){
-      if(a.href && a.href.indexOf('/blob/maindocs/source/') !== -1){
-        a.href = a.href.replace('/blob/maindocs/source/', '/blob/main/docs/source/');
+      if(a.href && a.href.indexOf('/blob/main/docs/source/') !== -1){
+        a.href = a.href.replace('/blob/main/docs/source/', '/blob/main/docs/source/');
       }
     });
   } catch (e) {
